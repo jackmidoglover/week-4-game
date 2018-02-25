873334184457ceaa962f1068d121eba5bd987e82
Jack <jackmidoglover@gmail.com>
2018-02-25 12:50:16 -0700

    js cleaned up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,12 +13,15 @@ var losses = 0;
 // game start function
 $(document).ready(function() {
 function gameStart() {
+    // these add values, mainly random numbers, to the globals
     targetGuess = Math.floor(Math.random() * (120 - 19) + 19);
     counter = 0;
     crystal1 = Math.floor((Math.random() * 12) + 1);
     crystal2 = Math.floor((Math.random() * 12) + 2);
     crystal3 = Math.floor((Math.random() * 12) + 3);
     crystal4 = Math.floor((Math.random() * 12) + 4);
+    //this prints variables to the DOM and adds a hidden number value to
+    // the crystal buttons
     $(".score").text(counter);
     $(".targetNumber").text(targetGuess);
     $("#citrine").attr("data-crystalvalue", crystal1);
@@ -27,7 +30,7 @@ function gameStart() {
     $("#jade").attr("data-crystalvalue", crystal4);
     $("#targetGuess").text(targetGuess);
 }
-
+// game restart function
 function newGame() {
     gameStart();
     counter = 0;
@@ -36,21 +39,23 @@ function newGame() {
 
 gameStart();
 crystalGame();
+// main game play
 function crystalGame() {
 
 $(".crystal-image").on("click", function(){
-    console.log($(this).attr("data-crystalvalue"));
+// if the counter is not already over the targetGuess,
 if (counter < targetGuess) {
+// retrieve data-crystal value, parseInt it and add it to counter
     counter += parseInt($(this).attr("data-crystalvalue"));
     $("#score").text(counter);
-
+//win condition
     if (counter === targetGuess) {
         wins++;
         $("#wins").text(wins);
         setTimeout(newGame, 1500);
 
       }
-  
+//lose condition
       else if (counter > targetGuess) {
         losses++;
         $("#loss").text(losses);
